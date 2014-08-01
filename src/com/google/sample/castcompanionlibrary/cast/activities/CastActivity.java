64ae0ac2e731616f3ec5c177ddaa409471f24ec9
Christophe Beyls <software@digitalia.be>
2014-08-01 22:03:10 +0200

    Added CastActivity as a parent class with VideoCastManager lifecycle callbacks and proper volume control. It also provides a default Cast action view in the Action Bar that can be optionally overridden.

---
+++ src/com/google/sample/castcompanionlibrary/cast/activities/CastActivity.java
@@ -0,0 +1,111 @@
+package com.google.sample.castcompanionlibrary.cast.activities;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.KeyEvent;
+import android.view.Menu;
+
+import com.google.android.gms.cast.MediaStatus;
+import com.google.sample.castcompanionlibrary.R;
+import com.google.sample.castcompanionlibrary.cast.BaseCastManager;
+import com.google.sample.castcompanionlibrary.cast.VideoCastManager;
+import com.google.sample.castcompanionlibrary.cast.exceptions.CastException;
+import com.google.sample.castcompanionlibrary.utils.Utils;
+
+/**
+ * All activities featuring Google Cast control should inherit from this activity.
+ * 
+ * @author Christophe Beyls
+ * 
+ */
+public class CastActivity extends ActionBarActivity {
+
+	public static final double DEFAULT_VOLUME_INCREMENT = 0.05;
+
+	private static double sVolumeIncrement = DEFAULT_VOLUME_INCREMENT;
+
+	protected VideoCastManager mCastManager;
+
+	/**
+	 * Call this method on application startup to override the default volume increment in all
+	 * activities inheriting from this one.
+	 * 
+	 * @param volumeIncrement
+	 */
+	public static void setVolumeIncrement(double volumeIncrement) {
+		sVolumeIncrement = volumeIncrement;
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		try {
+			mCastManager = VideoCastManager.getInstance(this);
+		} catch (CastException e) {
+		}
+	}
+
+	/**
+	 * You can override this method to either add menu items in addition to the cast button (by
+	 * calling this implementation first) or provide your own replacement menu items (by not calling
+	 * this implementation). If you choose the second option, you must call yourself
+	 * mCastManager.addMediaRouterButton(menu, R.id.media_route_menu_item);
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		getMenuInflater().inflate(R.menu.cast_player_menu, menu);
+		mCastManager.addMediaRouterButton(menu, R.id.media_route_menu_item);
+		return true;
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mCastManager.setContext(this);
+	}
+
+	@Override
+	protected void onDestroy() {
+		mCastManager.clearContext(this);
+		super.onDestroy();
+	}
+
+	@Override
+	public boolean dispatchKeyEvent(KeyEvent event) {
+		if (mCastManager.isConnected()) {
+			boolean isKeyDown = event.getAction() == KeyEvent.ACTION_DOWN;
+			switch (event.getKeyCode()) {
+			case KeyEvent.KEYCODE_VOLUME_UP:
+				if (onVolumeChange(sVolumeIncrement, isKeyDown)) {
+					return true;
+				}
+				break;
+			case KeyEvent.KEYCODE_VOLUME_DOWN:
+				if (onVolumeChange(-sVolumeIncrement, isKeyDown)) {
+					return true;
+				}
+				break;
+			}
+		}
+		return super.dispatchKeyEvent(event);
+	}
+
+	private boolean onVolumeChange(double volumeIncrement, boolean isKeyDown) {
+		if ((Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) && (mCastManager.isFeatureEnabled(BaseCastManager.FEATURE_LOCKSCREEN))
+				&& (mCastManager.getPlaybackStatus() == MediaStatus.PLAYER_STATE_PLAYING)) {
+			return false;
+		}
+
+		if (isKeyDown) {
+			try {
+				mCastManager.incrementVolume(volumeIncrement);
+			} catch (Exception e) {
+				Utils.showErrorDialog(this, R.string.failed_setting_volume);
+			}
+		}
+		return true;
+	}
+}
